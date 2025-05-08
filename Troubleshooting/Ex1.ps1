7a57b672d97c107f1eecde26f86c6ce4798da54a
roblevay <108578793+roblevay@users.noreply.github.com>
2025-05-08 17:35:45 +0200

    Update Ex1.ps1

--- Troubleshooting/Ex1.ps1
+++ Troubleshooting/Ex1.ps1
@@ -1,5 +1,27 @@
 Import-Module SqlServer
 $server = New-Object Microsoft.SqlServer.Management.Smo.Server "localhost"
+
+# Ensure login 'sqltom' exists and set password
+$loginName = "sqltom"
+$login = $server.Logins[$loginName]
+
+if (-not $login) {
+    $login = New-Object Microsoft.SqlServer.Management.Smo.Login($server, $loginName)
+    $login.LoginType = [Microsoft.SqlServer.Management.Smo.LoginType]::SqlLogin
+    $login.Create("myS3cret", $false)
+    $login.MustChangePassword = $false
+    $login.PasswordPolicyEnforced = $false
+    Write-Host "Login '$loginName' created."
+}
+else {
+    $login.ChangePassword("myS3cret")
+    Write-Host "Password for '$loginName' was updated."
+}
+
+# Switch to Windows Authentication mode
 $server.Settings.LoginMode = [Microsoft.SqlServer.Management.SMO.ServerLoginMode]::Integrated
 $server.Alter()
+
+# Stop SQL Server service
 Stop-Service -Name 'MSSQLSERVER' -Force
+
