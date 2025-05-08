81eee819354b7a60ac6f33bb2fc9f8986187c86e
roblevay <108578793+roblevay@users.noreply.github.com>
2025-05-08 17:51:13 +0200

    Update Ex1.ps1

--- Troubleshooting/Ex1.ps1
+++ Troubleshooting/Ex1.ps1
@@ -1,22 +1,13 @@
+# Import the sqlserver module
 Import-Module SqlServer
 $server = New-Object Microsoft.SqlServer.Management.Smo.Server "localhost"
 
-# Ensure login 'sqltom' exists and set password
-$loginName = "sqltom"
-$login = $server.Logins[$loginName]
+Invoke-Sqlcmd -ServerInstance "localhost" -Query @"
+IF EXISTS (SELECT 1 FROM sys.sql_logins WHERE name = 'sqltom')
+    DROP LOGIN sqltom;
+CREATE LOGIN sqltom WITH PASSWORD = 'myS3cret', CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF;
+"@ -TrustServerCertificate
 
-if (-not $login) {
-    $login = New-Object Microsoft.SqlServer.Management.Smo.Login($server, $loginName)
-    $login.LoginType = [Microsoft.SqlServer.Management.Smo.LoginType]::SqlLogin
-    $login.Create("myS3cret", $false)
-    $login.MustChangePassword = $false
-    $login.PasswordPolicyEnforced = $false
-    Write-Host "Login '$loginName' created."
-}
-else {
-    $login.ChangePassword("myS3cret")
-    Write-Host "Password for '$loginName' was updated."
-}
 
 # Switch to Windows Authentication mode
 $server.Settings.LoginMode = [Microsoft.SqlServer.Management.SMO.ServerLoginMode]::Integrated
