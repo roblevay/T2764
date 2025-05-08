cbb94f46e8a52a69a59a6b46951841bbac935266
roblevay <108578793+roblevay@users.noreply.github.com>
2025-05-08 18:45:56 +0200

    Update Ex1.ps1

--- Troubleshooting/Ex1.ps1
+++ Troubleshooting/Ex1.ps1
@@ -2,12 +2,20 @@
 Import-Module SqlServer
 $server = New-Object Microsoft.SqlServer.Management.Smo.Server "localhost"
 
+# Recreate login 'sqltom'
 Invoke-Sqlcmd -ServerInstance "localhost" -Query @"
 IF EXISTS (SELECT 1 FROM sys.sql_logins WHERE name = 'sqltom')
     DROP LOGIN sqltom;
 CREATE LOGIN sqltom WITH PASSWORD = 'myS3cret', CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF;
 "@ -TrustServerCertificate
 
+# Recreate login 'olle' and disable it
+Invoke-Sqlcmd -ServerInstance "localhost" -Query @"
+IF EXISTS (SELECT 1 FROM sys.sql_logins WHERE name = 'olle')
+    DROP LOGIN olle;
+CREATE LOGIN olle WITH PASSWORD = 'myS3cret', CHECK_POLICY = OFF, CHECK_EXPIRATION = OFF;
+ALTER LOGIN olle DISABLE;
+"@ -TrustServerCertificate
 
 # Switch to Windows Authentication mode
 $server.Settings.LoginMode = [Microsoft.SqlServer.Management.SMO.ServerLoginMode]::Integrated
