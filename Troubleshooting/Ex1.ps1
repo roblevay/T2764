381a7acf0f222779d5d795ad67fe5fa88cfc3434
roblevay <108578793+roblevay@users.noreply.github.com>
2025-05-08 13:55:55 +0200

    Update Ex1.ps1

--- Troubleshooting/Ex1.ps1
+++ Troubleshooting/Ex1.ps1
@@ -1,15 +1,5 @@
-# Laddar in SQL Server SMO (SQL Management Objects)
 Import-Module SqlServer
-
-# Koppla till instansen
-$server = New-Object Microsoft.SqlServer.Management.Smo.Server "DESKTOP-UTG2SVO"
-
-# Ändra till Windows Authentication only (1 = Windows Only, 2 = SQL + Windows)
+$server = New-Object Microsoft.SqlServer.Management.Smo.Server "localhost"
 $server.Settings.LoginMode = [Microsoft.SqlServer.Management.SMO.ServerLoginMode]::Integrated
-
-# Spara ändringen
 $server.Alter()
-
-# Stoppa SQL Server-tjänsten
-Stop-Service -Name 'MSSQLSERVER' -Force  # För defaultinstans
-# Eller t.ex. 'MSSQL$SQL2019' för namngiven instans
+Stop-Service -Name 'MSSQLSERVER' -Force
